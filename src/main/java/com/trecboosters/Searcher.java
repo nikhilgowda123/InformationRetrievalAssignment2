a875fbb6e8db8264f6305662158dab090beba637
YiruoZhang <81513392+YiruoZhang@users.noreply.github.com>
2022-12-08 18:56:17 +0000

    Improve performance of P5/MAP.

--- src/main/java/com/trecboosters/Searcher.java
+++ src/main/java/com/trecboosters/Searcher.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
@@ -20,11 +22,17 @@ import org.apache.lucene.search.BoostQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
+import org.apache.lucene.search.similarities.LMSimilarity;
+import org.apache.lucene.search.similarities.MultiSimilarity;
+import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.trecboosters.analyzer.CustomDocumentAnalyzer;
 import com.trecboosters.constants.CommonConstants;
 import com.trecboosters.model.QueryModel;
 import com.trecboosters.parser.Parser;
@@ -42,35 +50,73 @@ public class Searcher {
 			DirectoryReader directoryReader = DirectoryReader.open(directory);
 
 			IndexSearcher indexSearcher = new IndexSearcher(directoryReader);
-			indexSearcher.setSimilarity(Indexer.getSelectedSimilarityObject(selectedSimilarity));
-			Analyzer analyzer = Indexer.getSelectedAnalyzerObject(selectedAnalyser);
-
+			Similarity sm = null;
+			if(selectedSimilarity == null || selectedSimilarity.isEmpty())
+			{
+				sm = new BM25Similarity();
+			}
+			else
+			{
+				if("BM25".equalsIgnoreCase(selectedSimilarity))
+					sm = new BM25Similarity();
+				else if("Multi".equalsIgnoreCase(selectedSimilarity))
+				{
+					Similarity[] sims = {new BM25Similarity(), new LMJelinekMercerSimilarity(new LMSimilarity.DefaultCollectionModel(), 0.5f)};
+		            sm = new MultiSimilarity(sims);					
+				}
+				else
+				{
+					sm = new BM25Similarity();
+				}
+			}
+			indexSearcher.setSimilarity(sm);
+			
+			
+			Analyzer analyzer = new CustomDocumentAnalyzer();
+			
 			File resultFile = new File(CommonConstants.OUTPUT_FILE_PATH);
 			resultFile.getParentFile().mkdirs();
 			PrintWriter writer = new PrintWriter(resultFile, StandardCharsets.UTF_8.name());
-
 			ArrayList<QueryModel> queries = Parser.parseQuery(queryPath);
 
 			HashMap<String, Float> boosts = new HashMap<String, Float>();
-			boosts.put(CommonConstants.HEADLINE_TAG.toLowerCase(), (float) 3);
-
+			boosts.put(CommonConstants.HEADLINE_TAG.toLowerCase(), (float) 0.5);
+			boosts.put(CommonConstants.TEXT_TAG.toLowerCase(), (float) 10);
 			MultiFieldQueryParser queryParser = new MultiFieldQueryParser(
 					new String[] { CommonConstants.HEADLINE_TAG.toLowerCase(), CommonConstants.TEXT_TAG.toLowerCase() },
 					analyzer, boosts);
-
+			
 			NUM_RESULTS = numResults;
 			log.debug("Executing the fetched queries, max_hits set to: " + NUM_RESULTS);
 
 			for (QueryModel element : queries) {
 				Query titleQuery = queryParser.parse(QueryParser.escape(element.getTitle().trim()));
 				Query descriptionQuery = queryParser.parse(QueryParser.escape(element.getDesc().trim()));
-				Query narrativeQuery = queryParser.parse(QueryParser.escape(element.getNarr().trim()));
+				
+				List<String> splitNarrative = splitNarrIntoRelNotRel(QueryParser.escape(element.getNarr().trim()));
+                String relevantNarr = splitNarrative.get(0).trim();
+                String irrelevantNarr = splitNarrative.get(1).trim();
 
 				BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
-				booleanQueryBuilder.add(new BoostQuery(titleQuery, (float) 5), BooleanClause.Occur.SHOULD);
-				booleanQueryBuilder.add(new BoostQuery(descriptionQuery, (float) 3), BooleanClause.Occur.SHOULD);
-				booleanQueryBuilder.add(new BoostQuery(narrativeQuery, (float) 1), BooleanClause.Occur.SHOULD);
-
+				booleanQueryBuilder.add(new BoostQuery(titleQuery, (float) 6), BooleanClause.Occur.SHOULD);
+				booleanQueryBuilder.add(new BoostQuery(descriptionQuery, (float) 4.0), BooleanClause.Occur.SHOULD);
+				
+				 Query narrativeQuery = null;
+                 Query irrNarrativeQuery = null;
+                 if (relevantNarr.length() > 0) {
+                     narrativeQuery = queryParser.parse(QueryParser.escape(relevantNarr));
+                 }
+                 if (irrelevantNarr.length() > 0) {
+                     irrNarrativeQuery = queryParser.parse(QueryParser.escape(irrelevantNarr));
+                 }
+				
+                 if (narrativeQuery != null) {
+                	 booleanQueryBuilder.add(new BoostQuery(narrativeQuery, (float) 2.0), BooleanClause.Occur.SHOULD);
+                 }
+                 if (irrNarrativeQuery != null) {
+                	 booleanQueryBuilder.add(new BoostQuery(irrNarrativeQuery, (float) 1), BooleanClause.Occur.SHOULD);
+                 }
+				
 				search(indexSearcher, booleanQueryBuilder.build(), writer, element.getNum() ,queries.indexOf(element),
 						selectedAnalyser, selectedSimilarity);
 			}
@@ -84,6 +130,28 @@ public class Searcher {
 			log.error("Exception occured while running queries", e);
 		}
 	}
+	private static List<String> splitNarrIntoRelNotRel(String narrative) {
+        StringBuilder relevantNarr = new StringBuilder();
+        StringBuilder irrelevantNarr = new StringBuilder();
+        List<String> splitNarrative = new ArrayList<>();
+        BreakIterator bi = BreakIterator.getSentenceInstance();
+        bi.setText(narrative);
+        int index = 0;
+        while (bi.next() != BreakIterator.DONE) {
+            String sentence = narrative.substring(index, bi.current());
+            if (!sentence.contains("not relevant") && !sentence.contains("irrelevant")) {
+                relevantNarr.append(sentence.replaceAll(
+                        "a relevant document identifies|a relevant document could|a relevant document may|a relevant document must|a relevant document will|a document will|to be relevant|relevant documents|a document must|relevant|will contain|will discuss|will provide|must cite",
+                        ""));
+            } else {
+                irrelevantNarr.append(sentence.replaceAll("are also not relevant|are not relevant|are irrelevant|is not relevant|not|NOT", ""));
+            }
+            index = bi.current();
+        }
+        splitNarrative.add(relevantNarr.toString());
+        splitNarrative.add(irrelevantNarr.toString());
+        return splitNarrative;
+    }
 
 	public static void search(IndexSearcher is, Query query, PrintWriter writer, String queryNum, int queryID, String selectedAnalyser,
 			String selectedSimilarity) throws IOException {
